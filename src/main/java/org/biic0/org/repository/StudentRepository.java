a029eeda50f4ef74a1abd16080c278abc63df510
OyolaMpiko <222578629@mycput.ac.za>
2024-11-21 14:47:03 +0200

    Class name changes

--- src/main/java/org/biic0/org/repository/StudentRepository.java
+++ src/main/java/org/biic0/org/repository/StudentRepository.java
@@ -1,4 +1,10 @@
 package org.biic0.org.repository;
 
-public interface StudentRepository {
+import org.biic0.org.domain.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface StudentRepository extends JpaRepository<Student, String> {
+    List<Student> findStudentByStudentId(String studentId );
 }
--- src/main/java/org/biic0/org/repository/UserRepository.java
+++ src/main/java/org/biic0/org/repository/UserRepository.java
@@ -1,4 +1,10 @@
 package org.biic0.org.repository;
 
-public interface UserRepository {
+import org.biic0.org.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface UserRepository extends JpaRepository<User, String> {
+    List<User> findUserByEmail(String email);
 }
