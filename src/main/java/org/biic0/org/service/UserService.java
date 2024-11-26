149d874e574660b460de8deeeb4df36f0ee1b5e6
OyolaMpiko <222578629@mycput.ac.za>
2024-11-26 12:50:31 +0200

    Service and Controller classes

--- src/main/java/org/biic0/org/service/UserService.java
+++ src/main/java/org/biic0/org/service/UserService.java
@@ -1,4 +1,46 @@
 package org.biic0.org.service;
 
-public class UserService {
+import org.apache.catalina.User;
+import org.biic0.org.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService implements IUserService {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public org.biic0.org.domain.User create(org.biic0.org.domain.User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public org.biic0.org.domain.User read(String id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public org.biic0.org.domain.User update(org.biic0.org.domain.User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public org.biic0.org.domain.User update(User user) {
+        return null;
+    }
+
+    @Override
+    public List<org.biic0.org.domain.User> getAll() {
+        return userRepository.findAll();
+    }
+    public List<org.biic0.org.domain.User> findUserByEmail(String email) {
+        return userRepository.findUserByEmail(email);
+    }
 }
